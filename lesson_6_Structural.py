6c429586677826e7189128bcb505afe4f32940a6
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-25 20:18:24 +0400

    edited >=90

--- lesson_6_Structural.py
+++ lesson_6_Structural.py
@@ -15,14 +15,14 @@ if My_num1 > My_num2 and My_num1 > My_num3:
 
 weather = "hot"
 
-money = 100
+money = 90
 pool_price = 45
 
 len_of_weather = len(weather)
 
 if weather == 'hot' and money > pool_price and len_of_weather == 3:
 	print('go to the pool')
-	if money > 90:
+	if money >= 90:
 		print ("Invite a girl to go with you")
 
 
