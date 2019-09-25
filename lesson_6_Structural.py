e66d40ab4ccbfa55385b1ab530daa9955a8b31da
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-25 19:52:14 +0400

    1567rd commit

---
+++ lesson_6_Structural.py
@@ -0,0 +1,13 @@
+#lesson_6_Structural
+
+#Conditionals
+
+My_num1 = 8
+My_num2 = 4
+My_num3 = 5
+
+if My_num1 < My_num2 and My_num1 < My_num3:
+	print("First value is the least")
+if My_num1 > My_num2 and My_num1 > My_num3:
+	print("First value is the greatest")
+
