9259582f8f689947fba5bb434d0ecfdb5da87224
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-25 20:15:22 +0400

    nested

--- lesson_6_Structural.py
+++ lesson_6_Structural.py
@@ -8,6 +8,28 @@ My_num3 = 5
 
 if My_num1 < My_num2 and My_num1 < My_num3:
 	print("First value is the least")
+
 if My_num1 > My_num2 and My_num1 > My_num3:
 	print("First value is the greatest")
+
+
+weather = "cold"
+
+a = 5
+b = 3
+
+message = "hellow world"
+
+len_of_message = len(message)
+
+
+if weather == 'cold' and a > B and len_of_message > 3:
+	print('It works')
+
+print('our code ends')
+
+
+
+
+
 
