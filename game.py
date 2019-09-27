17b282b882523c2f9c50834f94839e18d423e688
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-27 20:34:06 +0400

    game

---
+++ game.py
@@ -0,0 +1,17 @@
+import random
+
+hidden_number = random.randint(1,100)
+
+user_guess = 0
+
+while not user_guess == hidden_number:
+	user_guess = int(input("Guess a number: "))
+	
+	if user_guess > hidden_number:
+		print("Too high!")
+	elif user_guess < hidden_number:
+		print("Too low!")
+	else:
+		print("Thats right")
+
+
