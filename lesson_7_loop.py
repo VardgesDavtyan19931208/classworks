66d28547b336290dfe92320a3cc0ef3c040b3a31
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-27 19:49:07 +0400

    average code

--- lesson_7_loop.py
+++ lesson_7_loop.py
@@ -1,13 +1,10 @@
-for i in range(0,10):
-	print(i)
-	if i > 5:
-		print("gg")
-	if i < 5:
-		print("ggwp")
+sum = 0
 
+for i in range(1,4):
+	next_number = int(input("Enter number # " + str(i) + ": "))
+	sum += next_number
 
-for b in range(10,0,-1):
-	print(b)
+print(sum/3)
 
 
 
