94d65caf6466b088526c516eadc23531508a13eb
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-27 20:11:43 +0400

    while

--- lesson_7_loop.py
+++ lesson_7_loop.py
@@ -7,4 +7,19 @@ for i in range(1,4):
 print(sum/3)
 
 
+text = "Hello World"
 
+for i in text:
+	print(i)
+
+
+some_string = ("hello world", "asdf", "asdasd")
+for i in some_string:
+	print(i)
+
+
+i = 0
+
+while i < 10:
+	print(i)
+	i += 1
