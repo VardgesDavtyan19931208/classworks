75c578609d6022f5307e719da0e49af90980bd52
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-27 21:17:37 +0400

    esiminch

--- game.py
+++ game.py
@@ -1,17 +1,65 @@
-import random
+#import random
 
-hidden_number = random.randint(1,100)
+#hidden_number = random.randint(1,100)
 
-user_guess = 0
+#user_guess = 0
 
-while not user_guess == hidden_number:
-	user_guess = int(input("Guess a number: "))
+#while not user_guess == hidden_number:
+#	user_guess = int(input("Guess a number: "))
 	
-	if user_guess > hidden_number:
-		print("Too high!")
-	elif user_guess < hidden_number:
-		print("Too low!")
-	else:
-		print("Thats right")
+#	if user_guess > hidden_number:
+#		print("Too high!")
+#	elif user_guess < hidden_number:
+#		print("Too low!")
+#	else:
+#		print("Thats right")
+
+
+#for i in range(1,11):
+#	for j in range(1,11):
+#		print(i, "sum", j, "equals", i+j)
+
+# for i in range(0,12):
+# 	if i % 2 == 0:
+# 		continue
+# 	print(i)
+
+
+# string = "abhgahah"
+
+# amount_of_js = 0
+
+# for i in string:
+# 	if i == "h":
+# 		amount_of_js += 1
+# print(i)
+
+# n = int(input("input the number: "))
+# number_of_even = 0
+
+# for i in range(0,n):
+# 	if i % 2 == 0:
+# 		number_of_even += 1
+# print(number_of_even)
+
+# amount = int(input("select a range"))
+
+# count = 0
+
+# for i in range(1, amount):
+# 	for j in range(2, amount):
+# 		if i % j == 0 and i != j:
+# 			break
+# 		if j == amount-1:
+# 			count += 1
+
+# print(count)
+
+
+
+
+
+
+
 
 
