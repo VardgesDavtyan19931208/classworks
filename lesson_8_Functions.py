bc01ead242673eeba70c10057a44876ab0c4c011
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-30 20:22:01 +0400

    def

---
+++ lesson_8_Functions.py
@@ -0,0 +1,49 @@
+# #def function name():
+# 	#return
+
+# def add_to_numbers(num1,num2):
+# 	num1 = int(num1)
+# 	num2 = int(num2)
+# 	return num1 + num2
+
+
+# first = 1
+# second = 2
+# third = 3
+
+
+# print(add_to_numbers(first,second), add_to_numbers(second,third))
+
+
+# print("Hello, I will help you to count the volume of prizm :))) ")
+
+
+# def prizm(num1,num2,num3):
+	
+# 	return num1*num2*num3
+
+
+# lenght = int(input("input the lenght: "))
+# weight = int(input("input the weight: "))
+# height = int(input("input the height: "))
+
+
+# print("The volume of prizm is",prizm(lenght,weight,height),"m^3")
+
+
+number = input("enter your number: ")
+
+def even_or_odd(number):
+	if number % 2 == 0:
+		return "even"
+	return "odd"
+
+print(even_or_odd(int(number)))
+
+
+
+
+
+
+
+
