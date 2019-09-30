307f7430dc58cb4b70ff8327f86b751523b98348
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-09-30 21:18:51 +0400

    final 8"

--- lesson_8_Functions.py
+++ lesson_8_Functions.py
@@ -31,14 +31,71 @@
 # print("The volume of prizm is",prizm(lenght,weight,height),"m^3")
 
 
-number = input("enter your number: ")
+# def validate_input(some_input):
+# 	if type(some_input) == int:
+# 		return True
+# 	else:
+# 		return False
 
-def even_or_odd(number):
-	if number % 2 == 0:
-		return "even"
-	return "odd"
+# while True:
+# 	user_input = input("Enter a integer")
+# 	print(validate_input(user_input))
+# 	if validate_input(user_input):
+# 		break
 
-print(even_or_odd(int(number)))
+# print(user_input)
+
+
+# number = input("enter your number: ")
+
+# def even_or_odd(number):
+# 	if number % 2 == 0:
+# 		return "even"
+# 	return "odd"
+
+# print(even_or_odd(int(number)))
+
+
+# def two_numbers(num1,num2):
+# 	return num1+num2
+
+# print(two_numbers(1,2))
+
+
+# print("string", "string2", end = "")
+# print("string" + "\n" + "string2")
+
+
+# def currency_amount(amount, currency="USD"):
+#     amount = str(amount)
+#     if currency == "JPY":
+#         return "¥" + amount
+#     elif currency == "USD":
+#         return "$" + amount    
+#     elif currency == "EUR":
+#         return "€" + amount
+#     else:
+#         return amount
+# print(currency_amount(5, "JPY"))
+
+
+
+my_balance = int(input("Input your balance: "))
+
+def check_balance(num1,num2):
+	return num1+num2
+
+price = int(input("Input the price: "))
+tax = int(input("Input the price: "))
+
+if check_balance(price, tax) > my_balance:
+	print("Go back home :( ")
+else:
+	print("Buy it!")
+
+
+
+
 
 
 
