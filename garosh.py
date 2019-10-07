022636933af1662950cb75f8de9f471141700544
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-07 21:28:08 +0400

    new

--- garosh.py
+++ garosh.py
@@ -17,12 +17,22 @@
 # print(a[-5:-2])
 
 
-my_string = "this is MY test string!"
+# my_string = "this is MY test string!"
 
 
-print(my_string)
-print(my_string.capitalize())
-print(my_string.lower())
-print(my_string.upper())
-print(my_string.title())
-print(my_string.replace("MY", "YOUR"))
+# print(my_string)
+# print(my_string.capitalize())
+# print(my_string.lower())
+# print(my_string.upper())
+# print(my_string.title())
+# print(my_string.replace("MY", "YOUR"))
+
+
+def get_middle_three_chars(sample_str):
+	middle_index = int(len(sample_str)/2)
+	print("Original String is", sample_str)
+	middle_three = sample_str[middle_index-1:middle_index+2]
+	print("Middle three chars are", middle_three)
+
+print(get_middle_three_chars("JhonDipPeta"))
+print(get_middle_three_chars("Jasonay"))
