0d69caff70cb2784f66271569a72d009264652e5
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-09 21:14:08 +0400

    tuple

--- lesson_13_tuple.py
+++ lesson_13_tuple.py
@@ -49,14 +49,61 @@
 # print(my_list, ": After inserting zero")
 
 
-def average(in_list):
-	sum = 0
-	for number in in_list:
-		sum += number
-	return sum/len(in_list)
+# def average(in_list):
+# 	sum = 0
+# 	for number in in_list:
+# 		sum += number
+# 	return sum/len(in_list)
 
-my_list1 = [1, 2, 3, 4, 5, 6, 7]
-my_list2 = [91, 92, 93, 94, 95, 96, 97]
+# my_list1 = [1, 2, 3, 4, 5, 6, 7]
+# my_list2 = [91, 92, 93, 94, 95, 96, 97]
 
-print("The average of my_list1 is: ", average(my_list1))
-print("The average of my_list1 is: ", average(my_list2))
+# print("The average of my_list1 is: ", average(my_list1))
+# print("The average of my_list1 is: ", average(my_list2))
+
+# def two_D_average(in_2d_list):
+# 	result = []
+
+# 	for num_list in in_2d_list:
+# 		sum = 0
+# 		for number in num_list:
+# 			sum += number
+
+# 		result.append(sum / len(num_list))
+
+# 	return result
+
+# my_2d_list = [[61, 32, 12, 123], [123,132, 131, 123], [4, 1, 2]]
+# print("averages: ", two_D_average(my_2d_list))
+
+
+# list_1 = [1,2,15,936,7,5,78,369,1106]
+
+# big = 0
+
+# for i in list_1:
+# 	if i > big:
+# 		big = i
+
+# print(big)
+
+
+
+list_a = [1,5,8,9,1,5,9,78,5,2,3,6,4,7,8,9,5,1,2,3,5,47,78,8,9,4,5,6,12,4,58,5,3,6,9,7,41]
+
+for i in list_a:
+	for b in list_a:
+		if i = b
+
+	list_a.remove()
+
+
+
+
+
+
+
+
+
+
+
