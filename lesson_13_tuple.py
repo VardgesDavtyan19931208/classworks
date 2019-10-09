d805b3dac4ca3affcd45831333c03e26b3b2b6d2
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-09 20:10:45 +0400

    tuple

--- lesson_13_tuple.py
+++ lesson_13_tuple.py
@@ -1,7 +1,7 @@
 def tuple1():
 	user_name = input("Enter your name: ")
 	user_surname = input("Enter your surname: ")
-	return user_name, user_surname
+	return [user_name, user_surname]
 
 user_info = tuple1()
 
@@ -9,7 +9,28 @@ print("Your name is " + user_info[0])
 print("Your surname us " + user_info[1])
 
 
-nested_tuple = ((1,2), (3,4), (5,6))
+nested_tuple = [[1,2], [3,4], [5,6]]
 
 print(nested_tuple[0])
 print(nested_tuple[0][0], nested_tuple[2][0])
+
+
+#Lists
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+
+my_list.remove(23)
+print(my_list, ": After removing 23")
+
+my_list.sort()
+print(my_list, ": After sorting")
+
+my_list.reverse()
+print(my_list, ": After reversing")
+
+my_list.pop()
+print(my_list, ": After poping")
+
+del my_list[-5:]
+print(my_list, ":After deleting the last five items")
