11babc3d3b9a982bfe4a53fb96eb095507c4c8d8
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-14 21:08:09 +0400

    i am back x2

--- lesson_15_dictionary.py
+++ lesson_15_dictionary.py
@@ -9,13 +9,37 @@
 # print(students_exam.values())
 
 
-classes = {"Math": ["David", "Lucy", "Dana"],
-			"Physics": ["Addison", "Benjamin"],
-			"Chemistery": ["Sara","Pele"]}
+# EX:4
+# classes = {"Math": ["David", "Lucy", "Dana"],
+# 			"Physics": ["Addison", "Benjamin"],
+# 			"Chemistery": ["Sara","Pele"]}
 
-print("Students in math class", classes["Math"])
+# print("Students in math class", classes["Math"])
 
-classes["Math"].append("Jirayr")
+# classes["Math"].append("Jirayr")
 
-print("Students in math class", classes["Math"])
+# print("Students in math class", classes["Math"])
 
+
+sample_text = "One morning when Gregor Samsa woke from troubled dreams he found himself transformed in his bed into a horrible vermin He lay on his armour-like back and if he lifted his head a little he could see his brown belly slightly domed and divided by arches into stiff sections The bedding was hardly able to cover it and seemed ready to slide off any moment His many legs pitifully thin compared with the size of the rest of him waved about helplessly as he looked Whats happened to me? he thought It wasn't a dream His room a proper human room although a little too small lay peacefully between its four familiar walls A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice gilded frame It showed a lady fitted out with a fur hat and fur boa who sat upright raising a heavy fur muff that covered the whole of her lower arm towards the viewer Gregor then turned to look out the window at the dull weather Drops"
+
+words_dict = {}
+sample_text = sample_text.lower()
+sample_text = sample_text.replace(',', '')
+sample_text = sample_text.replace('.', '')
+sample_text = sample_text.split(' ')
+
+
+
+for word in sample_text:
+	if word in words_dict.keys():
+		words_dict[word] += 1
+	else:
+		words_dict[word] = 1
+
+print(words_dict.items())
+
+
+# for (word,amount) in words_dict.items():
+# 	if amount > 2:
+# 		print(word, '.', amount)
