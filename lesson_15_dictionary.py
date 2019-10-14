d816f4926384532b50768a8a260c2df0219eb4e6
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-14 20:31:12 +0400

    i am back

---
+++ lesson_15_dictionary.py
@@ -0,0 +1,21 @@
+#students_exam = {'Gor Smbartyan': 26, 'David Grigoryan': 26, 'Vardges Hovhannisyan': 26, 'Rafayel Kostanyan': 28,'Khachatur Khachatryan': 23, 'Marat Yarumyan': 24, 'Artur Altunyan': 28,
+# 'Sedrak Harutyunyan': 24; 'Marianna Beglaryan': 25, 'Vardges Davtyan': 28, 'Tigran Danielyan': 28,'Arno Gevorgyan': 23, 'Artur Ananyan': 28, 'Arman Babayan': 28}
+
+# students_exam = {'Gor Smbartyan': 26, 'David Grigoryan': 26}
+
+# students_exam['Gor Smbartyan'] = 32
+
+# print(students_exam.keys())
+# print(students_exam.values())
+
+
+classes = {"Math": ["David", "Lucy", "Dana"],
+			"Physics": ["Addison", "Benjamin"],
+			"Chemistery": ["Sara","Pele"]}
+
+print("Students in math class", classes["Math"])
+
+classes["Math"].append("Jirayr")
+
+print("Students in math class", classes["Math"])
+
