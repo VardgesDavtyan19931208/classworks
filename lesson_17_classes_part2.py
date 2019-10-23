bcf2392dc5b4613c1d98ca5352d3f55627b8f6b5
vardges.davtyan.1993@gmail.com <ISTC@CREATE-3-1.istc.com>
2019-10-23 21:22:48 +0400

    range(len

---
+++ lesson_17_classes_part2.py
@@ -0,0 +1,99 @@
+# class Car:
+
+# 	def __init__(self, name, year):
+# 		self.name = name
+# 		self.year = year
+
+# 	def cars_name(self):
+# 		print("Name is" + self.name)
+
+# 	def cars_year(self):
+# 		print("Year is" + self.year)
+
+# class Car_child(Car):
+
+# 	def __init__(self, color, price):
+# 		self.color = color
+# 		self.price - price
+# 		super().__init__(self, name):
+# 		print()
+
+
+#  class BankAccount:
+#  	def __init__(self, amount):
+#  		self.amount = 1500
+
+#  	def amount1(self):
+#  		print("Amount is: {}".format(self.amount))
+
+#  	def set_amount(self, new_amount):
+#  		self.amount = new_amount
+
+
+# a = BankAccount()
+
+# a.amount1()
+
+
+# a.amount1 = 2000
+# a.amount1
+
+# a.set_amount(2000)
+
+
+# class Dog:
+# 	def sound(self):
+#  		print("Dogs are gavgav-ing")
+
+# class Cat:
+# 	def sound(self):
+#  		print("Cats are mewmew-ing")
+
+# def sound_test(animal):
+# 	animal.sound()
+
+# a = Dog()
+# b = Cat()
+
+# sound_test(a)
+# sound_test(b)
+
+
+# list1 = [1,2,3,4,5,6,7,8,9]
+# list2 = [10,11,12,13,14,15,16,17]
+# list3 = []
+
+# for i in list1:
+# 	if i % 2 == 0:
+# 		list3.append(i)
+
+# for y in list2:
+# 	if y % 2 != 0:
+# 		list3.append(y)
+
+# print(list3)
+
+
+string = (input("Please input text: "))
+
+string_a = string[::-1]
+
+for i in string_a:
+	for j in string:
+		if i == j:
+			print("True")
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
